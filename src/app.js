7f7d64a1ea4384dfccbb9bad45e81b83df6479d0
DineshBhuwad <dineshjbhuwad29@gmail.com>
2025-04-17 22:44:00 +0530

    testing different http requests

--- src/app.js
+++ src/app.js
@@ -3,17 +3,31 @@ const express = require("express")
 
 const app = express()  
 
-app.use('/dinesh',(req,res)=>{
-    res.send("Hello from Dinesh")
+// app.use('/dinesh',(req,res)=>{
+//     res.send("Hello from Dinesh")
+// })
+
+app.get('/test',(req,res)=>{
+    res.send({firstname : "Dinesh", lastname : "Bhuwad"})
 })
 
-app.use('/test',(req,res)=>{
-    res.send("Hello bitchess")
+app.post('/test',(req,res)=>{
+    //data saved successfully
+    res.send("Data saved successfully...")
+}) 
+
+app.delete('/test',(req,res)=>{
+    //data saved successfully
+    res.send("Data deleted successfully...")
 })
 
-app.use((req,res)=>{
-    res.send("Welcome to devTinder");
+app.patch("/test",(req,res)=>{
+    res.send("Patch Data send successfully...")
 })
+
+// app.use((req,res)=>{
+//     res.send("Welcome to devTinder");
+// })
 
 // app.use('/test',(req,res)=>{
 //     res.send("Hello bitchess")
